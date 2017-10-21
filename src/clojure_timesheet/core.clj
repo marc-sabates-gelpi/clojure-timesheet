bd6fcd09bd5cdf5b2c443c0ed8a9c975f87eb25a
Marc Sabates Gelpi <marc.sabates.gelpi.workspace@gmail.com>
2017-10-21 09:57:03 +0100

    Refactor

--- src/clojure_timesheet/core.clj
+++ src/clojure_timesheet/core.clj
@@ -10,27 +10,39 @@
 (def END "end")
 (def DEFAULT-DESC "default description")
 
+(defn start-session [ss file desc]
+  "Registers a new started session with the current time into the file"
+  (let [start (c/to-date (t/now))
+        new {:start start :desc (if (blank? desc) DEFAULT-DESC desc)}]
+    (spit file (conj ss new))
+    (println (str "Added new session: " new))))
+
+(defn end-session [ss file]
+  "Registers the end of the last started session with the current time into the file"
+  (let [end (c/to-date (t/now))
+        current (peek ss)
+        ended (assoc current :end end)]
+    (spit file (conj (pop ss) ended))
+    (println (str "Current ended session: " ended))))
+
+(defn sum-up-sessions [ss]
+  "Sums up the completed sessions time"
+  (-> (r/fold + (r/map #(t/in-minutes (t/interval
+                                       (c/from-date (:start %))
+                                       (c/from-date (:end %))))
+                       (r/filter #(and (:start %) (:end %)) ss)))
+      (as-> minutes
+          (/ minutes 60))
+      float
+      (as-> amount (format MESSAGE amount))
+      println))
+
 (defn -main
   [& [file op desc]]
   (-> file
       slurp
       read-string
       (as-> sessions
-          (cond (= op START) (let [start (pr-str (c/to-date (t/now)))
-                                   new-session {:start start :desc (if (blank? desc) DEFAULT-DESC desc)}]
-                               (spit file (conj sessions new-session))
-                               (str "Added new session: " new-session))
-                (= op END) (let [end (pr-str (c/to-date (t/now)))
-                                 current-session (peek sessions)
-                                 ended-session (assoc current-session :end end)]
-                             (spit file (conj (pop sessions) ended-session))
-                             (str "Ended session: " ended-session))
-                :else (-> (r/fold + (r/map #(t/in-minutes (t/interval
-                                                              (c/from-date (:start %))
-                                                              (c/from-date (:end %))))
-                                           (r/filter #(and (:start %) (:end %)) sessions)))
-                          (as-> minutes
-                              (/ minutes 60))
-                          float
-                          (as-> amount (format MESSAGE amount)))))
-      println))
+          (cond (= op START) (start-session sessions file desc) 
+                (= op END) (end-session sessions file)
+                :else (sum-up-sessions sessions)))))
